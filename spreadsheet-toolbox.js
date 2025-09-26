ceb314ebd06e10544998126decfe063ef6880abe
GFHK-SDGM <121885686+GFHK-SDGM@users.noreply.github.com>
2025-09-27 01:05:31 +0800

    Add various patterns.

--- spreadsheet-toolbox.js
+++ spreadsheet-toolbox.js
@@ -1,14 +1,19 @@
 "use strict";
 
 document.getElementById("match-btn").addEventListener("click", () => {
-  document.getElementById("replaced-str").value = document.getElementById("src-str").value.replace(new RegExp(document.getElementById("regex-pattern").value, "g"), document.getElementById("replace-value").value);
+  try {
+    document.getElementById("replaced-str").value = document.getElementById("src-str").value.replace(new RegExp(document.getElementById("regex-pattern").value, "gm"), document.getElementById("replace-value").value);
+  } catch (err) {
+    document.getElementById("replaced-str").value = "Error! Press F12 for error details.";
+    throw err;
+  };
 });
 
-document.getElementById("paste-btn").addEventListener("click", async () => {
+document.getElementById("paste-pattern-btn").addEventListener("click", async () => {
   document.getElementById("src-str").value = await navigator.clipboard.readText();
 });
 
-document.getElementById("copy-btn").addEventListener("click", () => {
+document.getElementById("copy-pattern-btn").addEventListener("click", () => {
   navigator.clipboard.writeText(document.getElementById("replaced-str").value);
 });
 
@@ -23,7 +28,7 @@ document.getElementById("pattern-preset").addEventListener("input", (ev) => {
       document.getElementById("replace-value").value = "";
       break;
     case "Whitespace trimming":
-      document.getElementById("regex-pattern").value = "";
+      document.getElementById("regex-pattern").value = "^\\s+|\\s+$";
       document.getElementById("replace-value").value = "";
       break;
     case "Remove unnecessary whitespaces between Chinese characters":
@@ -34,9 +39,40 @@ document.getElementById("pattern-preset").addEventListener("input", (ev) => {
       document.getElementById("regex-pattern").value = "@.+";
       document.getElementById("replace-value").value = "";
       break;
+    case "Separate class and class number":
+      document.getElementById("regex-pattern").value = "([PS]?\\d[A-za-z])\\s*(\\d{1,2})";
+      document.getElementById("replace-value").value = "$1";
+      break;
+    case "Separate Chinese and English":
+      document.getElementById("regex-pattern").value = "([A-Za-z-\\.]*)\\s*(.*)";
+      document.getElementById("replace-value").value = "$1";
+      break;
     default:
       document.getElementById("regex-pattern").value = "";
       document.getElementById("replace-value").value = "";
       break;
   };
+});
+
+document.getElementById("paste-level-btn").addEventListener("click", async () => {
+  document.getElementById("formname").value = await navigator.clipboard.readText();
+});
+
+document.getElementById("copy-level-btn").addEventListener("click", () => {
+  navigator.clipboard.writeText(document.getElementById("levels").value);
+});
+
+document.getElementById("calculate-levels").addEventListener("click", () => {
+  document.getElementById("levels").value = "";
+  document.getElementById("formname").value.split("\n").forEach((e) => {
+    try {
+      let level = parseInt(e) + parseInt(document.getElementById("level-offset").value);
+      if (level == 0)
+       level = "B3";
+      document.getElementById("levels").value += level + "\n";
+    } catch (err) {
+      document.getElementById("levels").value = "Error! Press F12 for error details.";
+      throw err;
+    };
+  });
 });
